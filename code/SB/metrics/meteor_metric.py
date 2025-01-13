7354236836a1bec02d29c562324233f22221f507
Lim Seong Beom <seongbeom1234@gmail.com>
2025-01-13 10:02:20 +0900

    modify meteor_metric.py

--- code/SB/metrics/meteor_metric.py
+++ code/SB/metrics/meteor_metric.py
@@ -1,20 +1,17 @@
 # meteor_metric.py
 from nltk.translate import meteor_score
+from nltk.corpus import wordnet as wn
 from transformers import AutoTokenizer
+from langchain_teddynote.community.kiwi_tokenizer import KiwiTokenizer
 
-# BERT 토크나이저 로딩
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-def tokenize(text):
-    """Hugging Face BERT 토크나이저를 사용한 토큰화"""
-    return tokenizer.tokenize(text)
 
 def calculate_meteor(question, answer):
-  
-  
-    reference_tokens = [tokenize(question)]  # 토큰화된 리스트
-    candidate_tokens = [tokenize(answer) ]   # 토큰화된 리스트
-
     
-    meteor = meteor_score.meteor_score(reference_tokens, candidate_tokens)
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+    wn.ensure_loaded()
+    meteor = meteor_score.meteor_score(
+    [tokenizer.tokenize(question)],
+    tokenizer.tokenize(answer),
+    )
     return meteor
