5fb11acd3e2964b9225b3f8b9a50303673f874ce
HoChangLee98 <ghckd3558@nvaer.com>
2025-02-04 15:16:27 +0900

    Feat : Good multiagent rag

--- graph_multiagentrag.py
+++ graph_multiagentrag.py
@@ -218,7 +218,7 @@ Ensure the integrity and consistency of the JSON data by strictly adhering to th
                 (
                     "system",
                     "You are a helpful AI assistant, collaborating with other assistants."
-                    " Use the provided tools to progress towards answering the question."
+                    " Use the provided tools to progress towards answering the question."                                        
                     " If you are unable to fully answer, that's OK, another assistant with different tools "
                     " will help where you left off. Execute what you can to make progress."
                     " If you or any of the other assistants have the final answer or deliverable,"
@@ -260,26 +260,37 @@ Ensure the integrity and consistency of the JSON data by strictly adhering to th
         # 그래프에서 도구를 실행하는 함수입니다.
         # 에이전트 액션을 입력받아 해당 도구를 호출하고 결과를 반환합니다.
         messages = state["messages"]
+        
         # 계속 조건에 따라 마지막 메시지가 함수 호출을 포함하고 있음을 알 수 있습니다.
+        first_message = messages[0]
         last_message = messages[-1]
+        
         # ToolInvocation을 함수 호출로부터 구성합니다.
-        tool_input = json.loads(
-            last_message.additional_kwargs["function_call"]["arguments"]
-        )
+        tool_input = json.loads(last_message.additional_kwargs["function_call"]["arguments"])
+        tool_name = last_message.additional_kwargs["function_call"]["name"]
+        
+        if tool_name == "retriever":
+            base_query = tool_input.get("__arg1", "")  # 기존 query 가져오기
+            refined_query = f"Context: {first_message.content} | Query: {base_query}"
+            tool_input["__arg1"] = refined_query
+        
         # 단일 인자 입력은 값으로 직접 전달할 수 있습니다.
         if len(tool_input) == 1 and "__arg1" in tool_input:
             tool_input = next(iter(tool_input.values()))
-        tool_name = last_message.additional_kwargs["function_call"]["name"]
+        
         action = ToolInvocation(
             tool=tool_name,
             tool_input=tool_input,
         )
+        
         # 도구 실행자를 호출하고 응답을 받습니다.
         response = self.tool_executor.invoke(action)
+        
         # 응답을 사용하여 FunctionMessage를 생성합니다.
         function_message = FunctionMessage(
             content=f"{tool_name} response: {str(response)}", name=action.tool
         )
+        
         # 기존 리스트에 추가될 리스트를 반환합니다.
         return {"messages": [function_message]}
     
